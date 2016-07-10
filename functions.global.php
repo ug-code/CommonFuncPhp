20146d25bdf3d9343f8361408075ce59928d6cba
ug-code <ugurgunes@windowslive.com>
2016-07-10 17:46:54 +0300

    Update functions.global.php

--- functions.global.php
+++ functions.global.php
@@ -1,5 +1,4 @@
 <?php  
-
 	/**
 	**
 	**@author:Uğur Güneş
@@ -7,26 +6,50 @@
 	**@since 2016-07-10
 	**/
 
-    function timer_start() {
 
-        global $timestart;
-        
-        $timestart = microtime( true );
-       
-        return true;
-    }
-    
-    function timer_stop( $display = 0, $precision = 3 ) {
-        
-        global $timestart, $timeend;
+	#My Function List 
+	interface Ifunc
+	{
+		function timer_start();
+		function timer_stop( $display, $precision);
+		function convert($size);
 
-        $timeend = microtime( true );
-        $timetotal = $timeend - $timestart;
-        $r = number_format( $timetotal, $precision );
+	}
 
-        if ( $display ) { echo nl2br("\n $r"); }
 
-        return $r;
-    }
-    
-    
+	#Let the game begin
+	class func implements Ifunc
+	{
+
+		function timer_start() {
+
+			global $timestart;
+
+			$timestart = microtime( true );
+
+			return true;
+		}
+
+		function timer_stop( $display = 0, $precision = 3 ) {
+
+			global $timestart, $timeend;
+
+			$timeend = microtime( true );
+			$timetotal = $timeend - $timestart;
+			$r = number_format( $timetotal, $precision );
+
+			if ( $display ) { echo nl2br("\n time: $r"); }
+
+			return $r;
+		}
+ 	
+		function convert($size)
+		{
+			$unit=array('b','kb','mb','gb','tb','pb');
+			return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+		}
+
+	}
+
+
+
