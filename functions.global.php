2d5550de19676631d662ae9a61a6de37b227c1b9
ug-code <ugurgunes@windowslive.com>
2016-07-10 17:19:22 +0300

    Update functions.global.php

--- functions.global.php
+++ functions.global.php
@@ -6,5 +6,27 @@
 	**@description : including a remote file in PHP
 	**@since 2016-07-10
 	**/
-	
-	
+
+    function timer_start() {
+
+        global $timestart;
+        
+        $timestart = microtime( true );
+       
+        return true;
+    }
+    
+    function timer_stop( $display = 0, $precision = 3 ) {
+        
+        global $timestart, $timeend;
+
+        $timeend = microtime( true );
+        $timetotal = $timeend - $timestart;
+        $r = number_format( $timetotal, $precision );
+
+        if ( $display ) { echo nl2br("\n $r"); }
+
+        return $r;
+    }
+    
+    
